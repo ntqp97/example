814d66da26cc81e2c012b169cbefbe32db4756c4
Phuoc Nguyen <ntqp97@gmail.com>
2018-06-11 15:43:53 +0700

    Added w_strlen function to return string lenght of wchar_t string

--- string.c
+++ string.c
@@ -1,45 +1,58 @@
 #include <stdio.h>
+#include <wchar.h>
+size_t w_strlen(const wchar_t *s)
+
+{
+
+	const wchar_t *p = s;
+
+
+	while (*p)
+		++p;
+	return (p - s);
+
+}
 
 size_t my_strlen(const char *s)
 {
-   char *p = s;
+	char *p = s;
 
-   while (*p)
-      ++p;
+	while (*p)
+		++p;
 
-   return (p - s);
+	return (p - s);
 }
 char *my_strcpy(char *t, char *s)
 
 {
-   
-   char *p = t;
-   
-   while (*t++ = *s++)
-    ;
-   
-   
-   return p;
-   
+
+	char *p = t;
+
+	while (*t++ = *s++)
+		;
+
+
+	return p;
+
 }
 
 int main(void)
 {
-   int i;
+	int i;
 
-   char p1[32];
-   char *s[] = 
-   {
-     	"Git tutorials",
-      	"Tutorials Point"
-   
+	char p1[32];
+	char *s[] =
+	{
+		"Git tutorials",
+		"Tutorials Point"
 
-   	for (i = 0; i < 2; ++i)
-      
-  	printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
 
-  	printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
+		for (i = 0; i < 2; ++i)
 
-   	return 0;
-   }
+			printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+		printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
+
+		return 0;
+	}
 }
